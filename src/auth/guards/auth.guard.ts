b0eebb523fcbea19e5a0096fec833a91dce99ff0
Shashwat <sshashwatssingh@gmail.com>
2025-07-22 19:37:15 +0530

    added guards and finished basic auth with JWT tokens

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -1,20 +1,34 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
+import { Request } from "express";
+
+interface JwtPayload {
+  sub: string;
+  username: string;
+}
+
+declare module 'express' {
+  interface Request {
+    user?: {
+      userId: string;
+      username: string;
+    };
+  }
+}
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor( private jwtService: JwtService ) {}
+    constructor(private jwtService: JwtService) {}
 
-    async canActivate(context: ExecutionContext) {
-
-        const request = context.switchToHttp().getRequest();
-        const authorization = request.headers.authorization; //Bearer <token>
-        const token = authorization?.split(' ')[1];
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const request = context.switchToHttp().getRequest<Request>();
+        const authorization: string | undefined = request.headers.authorization; //Bearer <token>
+        const token: string | undefined = authorization?.split(' ')[1];
 
         if (!token) throw new UnauthorizedException();
 
         try {
-            const tokenPayload = await this.jwtService.verifyAsync(token);
+            const tokenPayload = await this.jwtService.verifyAsync<JwtPayload>(token);
             request.user = {
                 userId: tokenPayload.sub,
                 username: tokenPayload.username
